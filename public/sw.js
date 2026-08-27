e20d7799d59da9893a489abe66ab28548c39b5a5
shengwansun34-source <shengwansun34@gmail.com>
2026-08-28 01:47:19 +0800

    Create sw.js

---
+++ public/sw.js
@@ -0,0 +1,9 @@
+const CACHE_NAME = 'polaris-v1';
+
+self.addEventListener('install', () => self.skipWaiting());
+self.addEventListener('activate', (event) => {
+  event.waitUntil(self.clients.claim());
+});
+self.addEventListener('fetch', (event) => {
+  event.respondWith(fetch(event.request));
+});
